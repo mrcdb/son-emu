b15b22936713b81f55703eb05a1c21ec69a48031
peusterm <manuel.peuster@uni-paderborn.de>
2017-10-25 14:25:48 +0200

    Jenkins re-run
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- devops-stages/stage-test.sh
+++ devops-stages/stage-test.sh
@@ -15,10 +15,4 @@ echo "Tests executed by user: $(whoami)"
 
 # trigger the tests
 cd /son-emu/
-pwd
-ls
-ls src/
-ls src/emuvim/
-ls src/emuvim/test/
-ls src/emuvim/test/unittest
-py.test -v src/emuvim/test/unittest
+py.test -v src/emuvim/test/unittests
