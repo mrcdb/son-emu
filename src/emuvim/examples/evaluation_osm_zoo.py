16e88171f93385d2d256163c2a0d5e17d451ab21
peusterm <manuel.peuster@uni-paderborn.de>
2017-12-22 13:56:30 +0100

    Telekom results
    Signed-off-by: peusterm <manuel.peuster@uni-paderborn.de>

--- src/emuvim/examples/evaluation_osm_zoo.py
+++ src/emuvim/examples/evaluation_osm_zoo.py
@@ -594,7 +594,7 @@ def main():
         osm_df.to_pickle("osm_{}".format(args.result_path))
     elif str(args.experiment).lower() == "service":
         #args.topology_list = ["Abilene.graphml", "DeutscheTelekom.graphml", "UsCarrier.graphml"]
-        args.topology_list = ["Abilene.graphml"]
+        args.topology_list = ["DeutscheTelekom.graphml"]
         args.zoo_path = "examples/topology_zoo/"
         args.max_services = 64 # 128(?)
         df, osm_df = run_service_experiments(args)
